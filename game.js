e2e2ffae4f51695541f04819da678385ce4020c0
patilarya76 <patilaryaanil07@gmail.com>
2023-11-03 10:08:52 +0530

    added successfully

--- game.js
+++ game.js
@@ -1,5 +1,98 @@
 // Iteration 2: Generate 2 random number and display it on the screen
+const num1= document.getElementById("number1");
+const num2 = document.getElementById("number2");
+
+let n1= Math.round(Math.random()*100);
+num1.innerHTML= n1;
+let n2 =Math.round(Math.random()*100);
+num2.innerHTML= n2;
 
 // Iteration 3: Make the options button functional
+const greaterThan= document.getElementById("greater-than");
+const equalTo= document.getElementById("equal-to");
+const lesserThan= document.getElementById("lesser-than");
+
+let score=0
+
+greaterThan.addEventListener("click",()=> {
+  if (n1>n2){
+    score++;
+    // score+=5
+    resetTimer(timerId)
+  }
+  else{
+        window.location.href="./gameover.html"
+    }
+   n1 = Math.round(Math.random()*100)
+  num1.innerHTML = n1;
+
+   n2 = Math.round(Math.random()*100)
+  num2.innerHTML = n2;
+
+}
+)
+
+equalTo.onclick =()=>{  
+    if (n1==n2){
+        score++
+        // score+=5
+        resetTimer(timerId)
+      }
+      else{
+            window.location.href="./gameover.html"
+        }
+       n1 = Math.round(Math.random()*100)
+      num1.innerHTML = n1;
+    
+       n2 = Math.round(Math.random()*100)
+      num2.innerHTML = n2;
+
+}
+
+lesserThan.addEventListener("click",()=> {
+    if (n1<n2){
+        score++
+        // score+=5
+        resetTimer(timerId)
+      }
+      else{
+            window.location.href="./gameover.html"
+        }
+       n1 = Math.round(Math.random()*100)
+      num1.innerHTML = n1;
+    
+       n2 = Math.round(Math.random()*100)
+      num2.innerHTML = n2;
+
+    
+}
+)
 
 // Iteration 4: Build a timer for the game
+const timer = document.getElementById("timer")
+var timerId; 
+// var time = 5;
+function startTimer(){
+    let time = 5;
+   timer.innerHTML= time;
+   timerId= setInterval(()=>{
+       time--
+       if(time==0){
+         window.location.href ="./gameover.html";
+
+        }
+        timer.innerHTML= time;
+    }, 1000);
+//   storethescore
+localStorage.setItem("squad56",score);
+
+}
+
+function resetTimer(timerId){
+     clearInterval(timerId) 
+    //  we are stopping the interval loop
+    startTimer()
+
+
+}
+startTimer()
--- score.js
+++ score.js
@@ -1,1 +1,14 @@
 // Iteration 5: Store the player score and display it on the game over screen
+const scoreBox = document.getElementById("score-board");
+
+// we will get the score from our walt
+let passcode = localStorage.getItem("squad56");
+console.log(passcode)
+
+scoreBox.innerHTML = passcode;
+
+const playAgainButton = document.getElementById("play-again-button")
+
+playAgainButton.onclick =()=>{
+    location.href="./game.html"
+}
